9609f3ce844e0b96936c2f759f8f1aee5bdca2d3
Alexander Myhin <myxin200015@mail.ru>
2020-10-23 13:12:39 +0300

    fix: refactoring main

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from Tasks import Tasks
-from timeit import Timer
 
-generate_by_name = False
-
-# n = int(input("Степень многочлена: "))
-# options_summary = int(input("Количество вариантов: "))
-# options_in_line = int(input("Количество вариантов в строке (2, 3): "))
-degree = 4
-options_summary = 13
-options_in_line = 3
+degree = int(input("Степень многочлена: "))
+options_summary = int(input("Количество вариантов: "))
+options_in_line = int(input("Количество вариантов в строке (2, 3): "))
 
 document = Tasks(options_summary, options_in_line, degree)
 document.generate()
