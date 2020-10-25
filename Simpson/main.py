b2dff5c7c5b6d87911276127cee66bb25dcc86bc
denisptuhov <denis.ptuhov.02@gmail.com>
2020-10-26 01:41:54 +0300

    Teper krasivo

--- Simpson/main.py
+++ Simpson/main.py
@@ -37,11 +37,14 @@ class TrapezoidTask:
         answerDoc.append(NewLine())
 
     def taskText(self):
-        return "1) Метод Трапеций"
+        return NoEscape("1) Вычислить приближённое значение " +
+                        r"$\int_{" + "{0:.1f}".format(self.xValues[0]) + "}^{" + "{0:.1f}".format(self.xValues[-1]) + "}f(x)dx$" \
+                        r"\hspace{1mm}от таблично заданной функции по формуле трапеций по шести и по девяти узлам. " \
+                        "Оценить погрешность по правилу Рунге; уточнить результат по Ричардсону.")
 
     def valueTable(self):
-        tableView = "|c|"
-        tableView += "c|" * len(self.xValues)
+        tableView = "|l|"
+        tableView += "l|" * len(self.xValues)
 
         table = Tabular(tableView)
         table.add_hline()
@@ -101,11 +104,14 @@ class SimpsonTask:
         answerDoc.append(NewLine())
 
     def taskText(self):
-        return "2) Метод Симпсона"
+        return NoEscape("2) Вычислить приближённое значение " +
+                        r"$\int_{" + "{0:.1f}".format(self.xValues[0]) + "}^{" + "{0:.1f}".format(self.xValues[-1]) + "}f(x)dx$" \
+                        r"\hspace{1mm}от таблично заданной функции по формуле Cимпсона по шести и по девяти узлам. " \
+                        "Оценить погрешность по правилу Рунге; уточнить результат по Ричардсону.")
 
     def valueTable(self):
-        tableView = "|c|"
-        tableView += "c|" * len(self.xValues)
+        tableView = "|l|"
+        tableView += "l|" * len(self.xValues)
 
         table = Tabular(tableView)
         table.add_hline()
@@ -144,11 +150,55 @@ def trapezoid(y, a, b, n):
     return res
 
 
-taskCnt = 9
-simpsonTasks = [SimpsonTask().randomize(9) for i in range(taskCnt)]
-trapezoidTasks = [TrapezoidTask().randomize(9) for j in range(taskCnt)]
+def createDocs(answerDoc, taskDoc, taskCnt):
+    simpsonTasks = [SimpsonTask().randomize(9) for i in range(taskCnt)]
+    trapezoidTasks = [TrapezoidTask().randomize(9) for j in range(taskCnt)]
 
-answerDoc = Document("answers", geometry_options={"lmargin": "0cm", "tmargin": "1cm"},
+    table = Tabular(" p{9cm} p{9cm} ")
+
+    addedVariantsCnt = 0
+    for i in range(0, taskCnt, 2):
+        addEmptySpace(table, 1)
+        table.add_row(["Вариант {0}".format(i + 1), "" if i + 1 == taskCnt else "Вариант {0}".format(i + 2)])
+
+        addEmptySpace(table, 1)
+
+        table.add_row(trapezoidTasks[i].taskText(), "" if i + 1 == taskCnt else trapezoidTasks[i + 1].taskText())
+        table.add_row(MultiRow(5, data=trapezoidTasks[i].valueTable()),
+                      MultiRow(5, data="" if i + 1 == taskCnt else trapezoidTasks[i + 1].valueTable()))
+
+        addEmptySpace(table, 4)
+
+        table.add_row(simpsonTasks[i].taskText(), "" if i + 1 == taskCnt else simpsonTasks[i + 1].taskText())
+        table.add_row(MultiRow(5, data=simpsonTasks[i].valueTable()),
+                      MultiRow(5, data="" if i + 1 == taskCnt else trapezoidTasks[i + 1].valueTable()))
+
+        addEmptySpace(table, 4)
+        addedVariantsCnt += 2
+
+        if addedVariantsCnt == 4:
+            addedVariantsCnt = 0
+            taskDoc.append(copy(table))
+            taskDoc.append(NewPage())
+            table.clear()
+
+    taskDoc.append(table)
+
+    for i in range(taskCnt):
+        answerDoc.append(NoEscape(r"\noindent {0}. ".format(i)))
+        trapezoidTasks[i].addTexAnswer(answerDoc)
+        simpsonTasks[i].addTexAnswer(answerDoc)
+        answerDoc.append(NewLine())
+
+
+def addEmptySpace(table, cnt):
+    for j in range(cnt):
+        table.add_empty_row()
+
+
+taskCnt = 10
+
+answerDoc = Document("answers",
                      documentclass=Command('documentclass', options=['a4paper'], arguments=['article']), page_numbers=False)
 
 taskDoc = Document("tasks", geometry_options={"lmargin": "1cm", "tmargin": "1cm"},
@@ -157,58 +207,7 @@ taskDoc = Document("tasks", geometry_options={"lmargin": "1cm", "tmargin": "1cm"
 answerDoc.packages.append(Package('babel', options=["russian"]))
 taskDoc.packages.append(Package('babel', options=["russian"]))
 
-table = Tabular("|c|c|")
-table.add_hline()
-
-for i in range(0, taskCnt, 2):
-    if i == taskCnt - 1:
-        table.add_row(["Вариант {0}".format(i), ""])
-        table.add_row(trapezoidTasks[i].taskText(), "")
-        table.add_row(MultiRow(5, data=trapezoidTasks[i].valueTable()),
-                      MultiRow(5, width="9cm"))
-
-        for j in range(4):
-            table.add_empty_row()
-
-        table.add_row(simpsonTasks[i].taskText(), "")
-        table.add_row(MultiRow(5, data=simpsonTasks[i].valueTable()),
-                      MultiRow(5, width="9cm"))
-        for j in range(4):
-            table.add_empty_row()
-        table.add_hline()
-        break
-
-    table.add_row(["Вариант {0}".format(i), "Вариант {0}".format(i + 1)])
-
-    table.add_row(trapezoidTasks[i].taskText(), trapezoidTasks[i + 1].taskText())
-    table.add_row(MultiRow(5, width="9cm", data=trapezoidTasks[i].valueTable()),
-                  MultiRow(5, width="9cm", data=trapezoidTasks[i + 1].valueTable()))
-
-    for j in range(4):
-        table.add_empty_row()
-
-    table.add_row(simpsonTasks[i].taskText(), simpsonTasks[i + 1].taskText())
-    table.add_row(MultiRow(5, width="9cm", data=simpsonTasks[i].valueTable()),
-                  MultiRow(5, width="9cm", data=simpsonTasks[i + 1].valueTable()))
-
-    for j in range(4):
-        table.add_empty_row()
-    table.add_hline()
-    if i == 6:
-        taskDoc.append(copy(table))
-        taskDoc.append(NewPage())
-        table.clear()
-        table.add_hline()
-
-
-taskDoc.append(table)
-
-
-for i in range(taskCnt):
-    answerDoc.append(NoEscape(r"\noindent {0}. ".format(i)))
-    trapezoidTasks[i].addTexAnswer(answerDoc)
-    simpsonTasks[i].addTexAnswer(answerDoc)
-    answerDoc.append(NewLine())
+createDocs(answerDoc, taskDoc, taskCnt)
 
 answerDoc.generate_tex()
 taskDoc.generate_tex()
