ccc2ad981141074927d24ff386deb53475b7c378
gagask <kholkovskii@mail.ru>
2020-11-08 02:31:15 +0300

    fix: add equal operator ti compare polynomials

--- Generator/interpolation/Polynomial.py
+++ Generator/interpolation/Polynomial.py
@@ -17,14 +17,17 @@ class Polynomial:
             self.coefficients = nprd.randint(-10, 11, degree + 1)
             x = np.arange(-20, 21)
             y = self.f(x)
-            tmp_x = x[np.logical_and(-60 <= y, y <= 60)]
+            x = x[np.logical_and(-60 <= y, y <= 60)]
 
             # если нам не хватает подходящих пар x и y заново генерим коэфициенты многочлена
-            if len(tmp_x) < degree + 1:
+            if len(x) < degree + 1:
                 continue
             else:
-                self.x = sorted(sample(tmp_x.tolist(), degree + 1))
+                self.x = sorted(sample(x.tolist(), degree + 1))
                 self.y = list(map(self.f, self.x))
+
+    def __eq__(self, obj):
+        return isinstance(obj, Polynomial) and obj.x == self.x and obj.y == self.y
 
     def f(self, x):
         y = 0
