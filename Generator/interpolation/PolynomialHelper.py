999b42c847f4c917ba2f472ba9315dbe835a4c78
Alexander Myhin <myxin200015@mail.ru>
2020-11-13 02:02:24 +0300

    fix: add check when seed is none

--- Generator/interpolation/PolynomialHelper.py
+++ Generator/interpolation/PolynomialHelper.py
@@ -1,10 +1,10 @@
-from Generator.interpolation.Polynomial import Polynomial
+from .Polynomial import Polynomial
 
 class PolynomialHelper:
     @staticmethod
     def generatePolynomial(degree, seed=None):
         if type(degree) == int and 3 <= degree <= 6 \
-                and type(seed) == int and 0 <= seed <= 2**32 - 1:
+                and ((type(seed) == int and 0 <= seed <= 2**32 - 1) or (seed is None)):
             return Polynomial(degree, seed)
         else:
             return None
