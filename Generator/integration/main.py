e3e41c2b17ff1cae858bae119880fc8896dc2d53
denisptuhov <denis.ptuhov.02@gmail.com>
2020-11-18 20:11:28 +0300

    first commit

--- Generator/integration/main.py
+++ Generator/integration/main.py
@@ -113,7 +113,7 @@ class SimpsonTask:
         return abs(self.halfAnswer - self.answer) / 15
 
     def taskText(self):
-        return NoEscape("1) Вычислить приближённое значение " +
+        return NoEscape("2) Вычислить приближённое значение " +
                         r"$\int_{" + "{0:.1f}".format(self.xValues[0]) + "}^{" + "{0:.1f}".format(self.xValues[-1]) + "}f(x)dx$" \
                         r"\hspace{1mm}от таблично заданной функции по формуле Симпсона по "
                         + d[int(self.n / 2) + self.n % 2] + " и по " + d[self.n] + " узлам." \
@@ -195,16 +195,25 @@ def fillTaskTables(table, firstTask, secondTask):
         addEmptySpace(table, 2)
 
 
-def createDocs(answerDoc, taskDoc, taskCnt, trapezoidTasks, simpsonTasks):
+def createDocs(answerDoc, taskDoc, taskCnt, trapezoidTasks, simpsonTasks, surnames=None):
+
     trapezoidTasks.append(None)
     simpsonTasks.append(None)
 
-    table = Tabular(" p{9cm} p{9cm} ")
+    if surnames is not None:
+        surnames.append(None)
+
+    table = Tabular(" |p{9cm}|p{9cm}| ")
+    table.add_hline()
 
     addedVariantsCnt = 0
     for i in range(0, taskCnt, 2):
         addEmptySpace(table, 1)
-        table.add_row(["Вариант {0}".format(i + 1), "" if i + 1 == taskCnt else "Вариант {0}".format(i + 2)])
+        if surnames is None:
+            table.add_row(["Вариант {0}".format(i + 1), "" if i + 1 == taskCnt else "Вариант {0}".format(i + 2)])
+        else:
+            table.add_row([surnames[i], "" if surnames[i + 1] is None else surnames[i + 1]])
+
         addEmptySpace(table, 1)
 
         table.add_row(trapezoidTasks[i].taskText(), "" if trapezoidTasks[i + 1] is None else trapezoidTasks[i + 1].taskText())
@@ -219,18 +228,25 @@ def createDocs(answerDoc, taskDoc, taskCnt, trapezoidTasks, simpsonTasks):
 
         if addedVariantsCnt == 4 and i + 2 < taskCnt:
             addedVariantsCnt = 0
+            table.add_hline()
             taskDoc.append(copy(table))
             taskDoc.append(NewPage())
             table.clear()
+        table.add_hline()
 
     taskDoc.append(table)
 
-    answerTable = Tabular(" p{9cm} p{9cm} ")
+    answerTable = Tabular("|p{9cm}|p{9cm}|")
+    answerTable.add_hline()
 
     addedVariantsCnt = 0
     for i in range(0, taskCnt, 2):
         addEmptySpace(answerTable, 1)
-        answerTable.add_row(["Вариант {0}".format(i + 1), "" if i + 1 == taskCnt else "Вариант {0}".format(i + 2)])
+        if surnames is None:
+            answerTable.add_row(["Вариант {0}".format(i + 1), "" if i + 1 == taskCnt else "Вариант {0}".format(i + 2)])
+        else:
+            answerTable.add_row([surnames[i], "" if surnames[i + 1] is None else surnames[i + 1]])
+
         addEmptySpace(answerTable, 1)
 
         fillTaskTables(answerTable, trapezoidTasks[i], trapezoidTasks[i + 1])
@@ -254,9 +270,11 @@ def createDocs(answerDoc, taskDoc, taskCnt, trapezoidTasks, simpsonTasks):
         addedVariantsCnt += 2
         if addedVariantsCnt == 4 and i + 2 < taskCnt:
             addedVariantsCnt = 0
+            answerTable.add_hline()
             answerDoc.append(copy(answerTable))
             answerDoc.append(NewPage())
             answerTable.clear()
+        answerTable.add_hline()
 
     answerDoc.append(answerTable)
 
@@ -281,12 +299,13 @@ def initDocs():
     return answerDoc, taskDoc
 
 
-def run(taskCnt, trapezoidsDotsCnt, simpsonDotsCnt, fileName, is_pdf, is_latex):
+def run(taskCnt, trapezoidsDotsCnt, simpsonDotsCnt, fileName, is_pdf, is_latex, surnames=None):
+
     simpsonTasks = [SimpsonTask().randomize(trapezoidsDotsCnt) for i in range(taskCnt)]
     trapezoidTasks = [TrapezoidTask().randomize(simpsonDotsCnt) for j in range(taskCnt)]
 
     answerDoc, taskDoc = initDocs()
-    createDocs(answerDoc, taskDoc, taskCnt, trapezoidTasks, simpsonTasks)
+    createDocs(answerDoc, taskDoc, taskCnt, trapezoidTasks, simpsonTasks, surnames)
 
     folder = 'interpolation_integration_generator/static/interpolation_integration_generator'
     if is_pdf:
--- Generator/interpolation/tests/interpolation_test.py
+++ Generator/interpolation/tests/interpolation_test.py
@@ -1,53 +1,15 @@
-from Generator.interpolation.PolynomialHelper import PolynomialHelper
+from Generator.interpolation.Polynomial import Polynomial
 import unittest
 
 
-class Test(unittest.TestCase):
-    def test_equals_with_same_seeds(self):
-        polynomial_1 = PolynomialHelper.generatePolynomial(3, 123)
-        polynomial_2 = PolynomialHelper.generatePolynomial(3, 5)
-        polynomial_3 = PolynomialHelper.generatePolynomial(3, 123)
-        polynomial_4 = PolynomialHelper.generatePolynomial(3, 5)
-
-        message = "First value and second value are not equal!"
-        self.assertEqual(polynomial_1, polynomial_3, message)
-        self.assertEqual(polynomial_2, polynomial_4)
-
-    def test_not_equals_with_different_seeds(self):
-        polynomial_1 = PolynomialHelper.generatePolynomial(3, 123)
-        polynomial_2 = PolynomialHelper.generatePolynomial(3, 5)
-        polynomial_3 = PolynomialHelper.generatePolynomial(4, 143)
-        polynomial_4 = PolynomialHelper.generatePolynomial(4, 342)
-
-        message = "First value and second value are equal!"
-        self.assertNotEqual(polynomial_1, polynomial_2, message)
-        self.assertNotEqual(polynomial_3, polynomial_4, message)
-
-    def test_none_polynomial(self):
-        polynomial_1 = PolynomialHelper.generatePolynomial(-1, 23)
-        polynomial_2 = PolynomialHelper.generatePolynomial(-1.3, 23)
-        polynomial_3 = PolynomialHelper.generatePolynomial(-1.3, "fasd")
-        polynomial_4 = PolynomialHelper.generatePolynomial(3, 2.34)
-        polynomial_5 = PolynomialHelper.generatePolynomial(5, -42134)
-
-        message = "Object in not none!"
-        self.assertIsNone(polynomial_1, message)
-        self.assertIsNone(polynomial_2, message)
-        self.assertIsNone(polynomial_3, message)
-        self.assertIsNone(polynomial_4, message)
-        self.assertIsNone(polynomial_5, message)
-
-    def test_not_none_polynomial(self):
-        polynomial_1 = PolynomialHelper.generatePolynomial(3, 234)
-        polynomial_2 = PolynomialHelper.generatePolynomial(4, 0)
-        polynomial_3 = PolynomialHelper.generatePolynomial(5, 2**32-1)
-        polynomial_4 = PolynomialHelper.generatePolynomial(6, 7543)
-
-        message = "Object is none!"
-        self.assertIsNotNone(polynomial_1, message)
-        self.assertIsNotNone(polynomial_2, message)
-        self.assertIsNotNone(polynomial_3, message)
-        self.assertIsNotNone(polynomial_4, message)
+class TestSameData(unittest.TestCase):
+    # test function to test equality of two value
+    def test_negative(self):
+        first_polynomial = Polynomial(3, 123)
+        second_polynomial = Polynomial(3, 5)
+        third_polynomial = Polynomial(3, 123)
+        message = "First value and second value are not equal !"
+        self.assertEqual(first_polynomial, third_polynomial, message)
 
 
 if __name__ == '__main__':
