b4afce96e5c2fdee4fd9e4cdad68b4adaad242b4
denisptuhov <denis.ptuhov.02@gmail.com>
2020-11-20 15:05:28 +0300

    first commit

--- Generator/Generator/settings.py
+++ Generator/Generator/settings.py
@@ -19,11 +19,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'interpolation_integration_generator',
+    'interpolation',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +59,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Generator.wsgi.application'
 
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -74,6 +89,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -86,6 +102,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- Generator/interpolation_integration_generator/views.py
+++ Generator/interpolation_integration_generator/views.py
@@ -1,94 +1,80 @@
-import os
 import zipfile
-from datetime import datetime
 from os.path import basename
+from django.views.decorators.csrf import csrf_exempt
 
 from django.http import HttpResponse
 from django.shortcuts import render
-from integration.main import run
 from interpolation.Tasks import Tasks
+from integration.main import run
 
 
 def index(request):
     return render(request, "interpolation_integration_generator/index.html", {})
 
 
-async def generate_interpolations(request):
-    names = []
-    files = []
-    sizes = []
+@csrf_exempt
+def generate_interpolations(request):
+    options_in_line = int(request.POST.get("number"))
+    degree = int(request.POST.get("degree"))
 
-    timestamp = str(datetime.now()).replace(":", "-").replace(" ", "_")
-    folder = f'interpolation_integration_generator/static/interpolation_integration_generator/{timestamp}'
-    static_folder = f"/static/interpolation_integration_generator/{timestamp}"
-    os.mkdir(f"{folder}")
-
-    options_in_line = int(request.GET.get("number"))
-    degree = int(request.GET.get("degree"))
-    options_summary = int(request.GET.get("options_summary"))
-
-    is_pdf = True if request.GET.get("saveOnPDF") == "Yes" else False
-    is_latex = True if request.GET.get("saveOnLaTex") == "Yes" else False
-    filename = request.GET.get("filename")
+    is_pdf = True if request.POST.get("saveOnPDF") == "Yes" else False
+    is_latex = True if request.POST.get("saveOnLaTex") == "Yes" else False
+    filename = request.POST.get("filename")
     seed = None
     try:
-        seed = int(request.GET.get("seed"))
+        seed = int(request.POST.get("seed"))
     except ValueError:
         pass
 
-    document = Tasks(options_summary, options_in_line, degree, seed)
-    document.generate(filename, is_pdf, is_latex, timestamp)
+    variantsType = request.POST.get("Numbering")
 
+    surnames = None
+    options_summary = 0
+    if variantsType == "Digits":
+        options_summary = request.POST.get("options_summary")
+    else:
+        surnames = request.FILES['surnameFile'].read().decode("utf-8").splitlines()
+        options_summary = len(surnames)
+
+    document = Tasks(options_summary, options_in_line, degree, seed)
+    document.generate(filename, is_pdf, is_latex, surnames)
+
+    folder = 'interpolation_integration_generator/static/interpolation_integration_generator'
     filenames = []
     if is_pdf:
         filenames.append(f"{folder}/{filename}.pdf")
         filenames.append(f'{folder}/answers_for_{filename}.pdf')
 
-        names.append(f"{filename}.pdf")
-        files.append(f"{static_folder}/{filename}.pdf")
-        sizes.append(os.path.getsize(f"{folder}/{filename}.pdf"))
-
-        names.append(f"answers_for_{filename}.pdf")
-        files.append(f"{static_folder}/answers_for_{filename}.pdf")
-        sizes.append(os.path.getsize(f"{folder}/answers_for_{filename}.pdf"))
-
     if is_latex:
         filenames.append(f"{folder}/{filename}.tex")
         filenames.append(f'{folder}/answers_for_{filename}.tex')
 
-        names.append(f"{filename}.tex")
-        files.append(f"{static_folder}/{filename}.tex")
-        sizes.append(os.path.getsize(f"{folder}/{filename}.tex"))
-
-        names.append(f"answers_for_{filename}.tex")
-        files.append(f"{static_folder}/answers_for_{filename}.tex")
-        sizes.append(os.path.getsize(f"{folder}/answers_for_{filename}.tex"))
-
     with zipfile.ZipFile(f'{folder}/result.zip', 'w') as zipObj:
         for file in filenames:
             zipObj.write(file, basename(file))
+    response = HttpResponse(open(f'{folder}/result.zip', 'rb'))
+    response['Content-Type'] = 'application/x-zip-compressed'
+    response['Content-Disposition'] = f'attachment; filename="{folder}/result.zip"'
 
-    names.append("result.zip")
-    files.append(f"{static_folder}/result.zip")
-
-    sizes = list(map(lambda size: round(size/1024, 1), sizes))
-    sizes.append(os.path.getsize(f"{folder}/result.zip"))
-
-    context = {'files': zip(names, files, sizes)}
-
-    return render(request, "interpolation_integration_generator/result_page.html", context=context)
+    return response
 
 
+@csrf_exempt
 def generate_integration(request):
-    variantsCnt = int(request.GET.get("variantsCnt"))
-    TrapezoidPointsCnt = int(request.GET.get("TrapezoidPointsCnt"))
-    SimpsonPointsCnt = int(request.GET.get("SimpsonPointsCnt"))
+    TrapezoidPointsCnt = int(request.POST.get("TrapezoidPointsCnt"))
+    SimpsonPointsCnt = int(request.POST.get("SimpsonPointsCnt"))
 
-    is_pdf = True if request.GET.get("saveOnPDF") == "Yes" else False
-    is_latex = True if request.GET.get("saveOnLaTex") == "Yes" else False
-    filename = request.GET.get("fileName")
+    is_pdf = True if request.POST.get("saveOnPDF") == "Yes" else False
+    is_latex = True if request.POST.get("saveOnLaTex") == "Yes" else False
+    filename = request.POST.get("fileName")
 
-    run(variantsCnt, SimpsonPointsCnt, TrapezoidPointsCnt, filename, is_pdf, is_latex)
+    variantsType = request.POST.get("Numbering")
+
+    if variantsType == "Digits":
+        run(int(request.POST.get("variantsCnt")), SimpsonPointsCnt, TrapezoidPointsCnt, filename, is_pdf, is_latex)
+    else:
+        surnames = request.FILES['surnameFile'].read().decode("utf-8").splitlines()
+        run(len(surnames), SimpsonPointsCnt, TrapezoidPointsCnt, filename, is_pdf, is_latex, surnames)
 
     folder = 'interpolation_integration_generator/static/interpolation_integration_generator'
     filenames = []
