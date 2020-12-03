e397886a3e153e4a7c7ffce9a07c60f731883de0
Alexander Myhin <myxin200015@mail.ru>
2020-12-03 14:31:12 +0300

    fix: add suchkok requirements

--- Generator/interpolation/Polynomial.py
+++ Generator/interpolation/Polynomial.py
@@ -5,16 +5,20 @@ class Polynomial:
     degree = None
 
     def __init__(self, degree, seed=None):
+        self.degree = degree
+        step = random.choice([2, 3, 4])
         if seed is not None:
             random.seed(seed)
-        self.x = []
+        self.x = [0, 1]
         self.y = []
 
-        while len(self.x) != degree + 1:
+        while True:
+            if len(self.x) == degree + 1 and self.check_difference(step):
+                break
             self.coefficients = random.randint(-10, 11, degree + 1)
-            x = np.arange(-10, 11)
+            x = np.arange(-10, 51)
             y = self.f(x)
-            x = x[np.logical_and(-50 <= y, y <= 50)]
+            x = x[np.logical_and(-500 <= y, y <= 500)]
 
             # если нам не хватает подходящих пар x и y заново генерим коэфициенты многочлена
             if len(x) < degree + 1:
@@ -31,3 +35,9 @@ class Polynomial:
         for i, coefficient in enumerate(self.coefficients):
             y += coefficient * pow(x, i)
         return y
+
+    def check_difference(self, difference):
+        for i in range(1, len(self.x)):
+            if self.x[i] - self.x[i-1] != difference:
+                return False
+        return True
